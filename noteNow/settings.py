4f48f62c98a96e80a245595c075c21182dcd822f
avemike <mocard2000@gmail.com>
2022-01-22 22:44:39 +0100

    feat: add simple seed

--- noteNow/settings.py
+++ noteNow/settings.py
@@ -55,6 +55,16 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+)
+
 ROOT_URLCONF = 'noteNow.urls'
 
 TEMPLATES = [
---
+++ src/management/commands/seed.py
@@ -0,0 +1,66 @@
+from django.core.management.base import BaseCommand
+from src.models import User, Note, Segment
+from django.contrib.auth.hashers import make_password
+# python manage.py seed --mode=refresh
+
+""" Clear all data and seed """
+MODE_REFRESH = 'refresh'
+
+""" Clear all data  """
+MODE_CLEAR = 'clear'
+
+
+class Command(BaseCommand):
+    help = "seed database for testing and development."
+
+    def add_arguments(self, parser):
+        parser.add_argument('--mode', type=str, help="Mode")
+
+    def handle(self, *args, **options):
+        self.stdout.write('seeding data...')
+        run_seed(self, options['mode'])
+        self.stdout.write('done.')
+
+
+def clear_data():
+    """Deletes all the table data"""
+    User.objects.all().delete()
+    Note.objects.all().delete()
+    Segment.objects.all().delete()
+
+
+def create_user():
+    """Creates an User and fills his data"""
+    user = User(
+        firstName='John',
+        lastName='Doe',
+        email='john@doe.com',
+        hash=make_password('Password12!')
+    )
+    user.save()
+
+    note = Note(
+        name='First note',
+        owner=user
+    )
+    note.save()
+
+    segment = Segment(
+        content='Very first paragraph',
+        order=0
+    )
+    segment.save()
+
+
+def run_seed(self, mode):
+    """ Seed database based on mode
+
+    :param mode: refresh / clear 
+    :return:
+    """
+    # Clear data from tables
+    clear_data()
+    if mode == MODE_CLEAR:
+        return
+
+    create_user()
