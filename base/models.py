5018924c9944f33f16aff8ff79061c3ef5d6e8c8
avemike <mocard2000@gmail.com>
2022-02-01 18:51:51 +0100

    refactor: clean up the codebase

--- base/models.py
+++ base/models.py
@@ -4,29 +4,6 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 DEFAULT_NOTE_ID = 1
 
 
-class MyAccountManager(BaseUserManager):
-    def create_user(self, email, password=None):
-        if not email:
-            raise ValueError('Users must have an email address')
-
-        user = self.model(
-            email=self.normalize_email(email))
-
-        user.set_password(password)
-        user.save(using=self._db)
-
-        return user
-
-    def create_admin(self, email, password):
-        user = self.create_user(
-            email=self.normalize_email(email),
-            password=password,
-        )
-        user.is_admin = True
-
-        user.save(using=self._db)
-
-
 class User(AbstractUser):
     email = models.EmailField(verbose_name="email", max_length=60,
                               unique=True, blank=False, null=False, default=None)
@@ -42,13 +19,10 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    objects = MyAccountManager()
-
     def __str__(self):
         return str(self.email)
 
-    def has_perm(self, perm, obj=None): return self.is_admin
-    def has_module_perms(self, app_label): return self.is_admin
+    def has_perm(self): return self.is_admin
 
 
 class Note(models.Model):
